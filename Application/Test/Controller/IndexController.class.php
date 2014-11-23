f895285beee5313f9eddd7077992c567002d861f
dd <dd>
2014-11-23 21:42:37 +0800

    测试RPC

--- Application/Test/Controller/IndexController.class.php
+++ Application/Test/Controller/IndexController.class.php
@@ -2,7 +2,7 @@
 
 /**
  * 测试
- * @author Administrator
+ * @author CGY
  *
  */
 
@@ -13,4 +13,26 @@ class IndexController extends \Think\Controller {
 	public function indexAct(){
 		$this->display();
 	}
+	
+	public function phpRPCAct(){
+		Vendor('phpRPC.phprpc_client');
+		$client = new \PHPRPC_Client('http://localhost:8500/Api/RpcTest');
+		$result = $client->test();
+		dump($result);
+	}
+	
+	public function hproseAct(){
+		vendor('Hprose.HproseHttpClient');
+		$client = new \HproseHttpClient('http://localhost:8500/Api/HproseTest');
+		$result = $client->test();
+		dump($result);
+	}
+	
+	public function jsonRPCAct(){
+		vendor('jsonRPC.jsonRPCClient');
+		$client = new \jsonRPCClient('http://localhost:8500/Api/JsonRPCTest');
+		$result = $client->test();
+		var_dump($result); // 结果：
+	}
+	
 }
