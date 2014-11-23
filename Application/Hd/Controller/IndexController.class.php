2830347cd27af151a42d5256d83252dc8842a49d
dd <dd>
2014-11-23 13:09:13 +0800

    初始版本

---
+++ Application/Hd/Controller/IndexController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 测试
+ * @author CGY
+ *
+ */
+
+namespace Hd\Controller;
+
+class IndexController extends \Think\Controller {
+	
+	public function indexAct(){
+		$this->display();
+	}
+}
---
+++ Application/Test/Controller/IndexController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 测试
+ * @author Administrator
+ *
+ */
+
+namespace Test\Controller;
+
+class IndexController extends \Think\Controller {
+	
+	public function indexAct(){
+		$this->display();
+	}
+}
