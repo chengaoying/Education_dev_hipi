8fc0afb947ab405260c046279b843f886476c858
dd <dd>
2014-12-15 09:05:27 +0800

    12

--- Runtime/Cache/Hd/2037dec581471a39ce0184eed6d28cec.php
+++ Runtime/Cache/Hd/2037dec581471a39ce0184eed6d28cec.php
@@ -149,7 +149,7 @@ window.onload=function()
 <!-- 页面右侧 -->
 	<!-- 推荐的课程 -->
 
-<?php $__FOR_START_20824__=1;$__FOR_END_20824__=7;for($i=$__FOR_START_20824__;$i < $__FOR_END_20824__;$i+=1){ if($i > 3){ $top = 405; $left = 540 + ($i-4)*225; }else{ $top = 180; $left = 540 + ($i-1)*225; } ?>
+<?php $__FOR_START_11383__=1;$__FOR_END_11383__=7;for($i=$__FOR_START_11383__;$i < $__FOR_END_11383__;$i+=1){ if($i > 3){ $top = 405; $left = 540 + ($i-4)*225; }else{ $top = 180; $left = 540 + ($i-1)*225; } ?>
 	<div id="div_tuijian_<?php echo ($i); ?>" style="position:absolute;width:210px;height:210px;left:<?php echo ($left); ?>px;top:<?php echo ($top); ?>px;vertical-align:middle;text-align:center;">
 		<img id="tuijian_<?php echo ($i); ?>" src="/static/v1/hd/images/test/<?php echo ($i); ?>.jpg" width="210" height="210">
 	</div><?php } ?>
